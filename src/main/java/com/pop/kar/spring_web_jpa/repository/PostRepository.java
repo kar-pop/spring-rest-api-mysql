3c8e80012c3f03f1d7a478a644ca7daaa211f353
kar-pop <enig123@wp.pl>
2025-07-10 12:40:57 +0200

    Dodanie PostRepository

---
+++ src/main/java/com/pop/kar/spring_web_jpa/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.pop.kar.spring_web_jpa.repository;
+
+import com.pop.kar.spring_web_jpa.entitiy.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    //dsafsdfsdf
+}
