8b0e9813b9c3bd7a5a273f9feac170662836a036
kar-pop <enig123@wp.pl>
2025-07-10 14:54:33 +0200

    Finally added SpringBootAplication...

---
+++ src/main/java/com/pop/kar/spring_web_jpa/SpringWebJpaApplication.java
@@ -0,0 +1,14 @@
+package com.pop.kar.spring_web_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebJpaApplication.class, args);
+
+	}
+
+}
